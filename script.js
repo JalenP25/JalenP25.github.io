c9217161f6118dd52538a9a2c35f29bc5fec3fcd
Jalen Peeler <jalenpeeler@yahoo.com>
2021-03-27 06:14:12 -0400

    updates, added button, added script

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function mouseOver() {
 function mouseOut() {
     document.getElementById("click").style.color = "black";
 }
+
+function wantMore() {
+    const bio = document.createElement("footer");
+    bio.setAttribute("id", "education");
+    document.body.appendChild(bio);
+
+    const header = document.createElement("h3");
+    const title = document.createTextNode("Education");
+    header.appendChild(title);
+
+    document.getElementById("education").appendChild(header);
+
+    const para = document.createElement("p");
+    const text = document.createTextNode("I went to UNC Charlotte to study Psychology and computer science.");
+    para.appendChild(text);
+
+    document.getElementById("education").appendChild(para);
+}

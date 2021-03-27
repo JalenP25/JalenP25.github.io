18d56bbe8be1ad71b14784711f8147705f43bbb3
Jalen Peeler <jalenpeeler@yahoo.com>
2021-03-27 16:28:44 -0400

    added blog link to button

--- script.js
+++ script.js
@@ -15,14 +15,16 @@ function wantMore() {
     document.body.appendChild(bio);
 
     const header = document.createElement("h3");
-    const title = document.createTextNode("Education");
+    const title = document.createTextNode("Blog");
     header.appendChild(title);
 
     document.getElementById("education").appendChild(header);
 
-    const para = document.createElement("p");
-    const text = document.createTextNode("I went to UNC Charlotte to study Psychology and computer science.");
-    para.appendChild(text);
+    const blogBio = document.createElement("a");
+    const text = document.createTextNode("Through The Ages");
+    blogBio.setAttribute("href", "https://jalenpeeler.medium.com/through-the-ages-7d48c89f60b2");
+    blogBio.appendChild(text);
+    document.body.appendChild(blogBio);
 
-    document.getElementById("education").appendChild(para);
+    //document.getElementById("education").appendChild(para);
 }

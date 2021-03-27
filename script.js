8ea4270a2b4dd5736ec1b44a7f7e6bd79870f3da
Jalen Peeler <jalenpeeler@yahoo.com>
2021-03-27 05:44:11 -0400

    updates, ids, and javascript

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function createCardFromCity(city) {
-    const card = document.createElement("div");
-    card.className = "card";
-    card.innerHTML = 
-    `<h3>${city.name}</h3>
-    <small>population: ${city.population}</small>
-    <p>You'll want to go see the ${city.attraction}</p>
-    `
-    return card
-  }
+document.getElementById("click").addEventListener("mouseover", mouseOver);
+document.getElementById("click").addEventListener("mouseout", mouseOut);
+
+function mouseOver() {
+    document.getElementById("click").style.color = "blue";
+}
+
+function mouseOut() {
+    document.getElementById("click").style.color = "black";
+}

c3ecb6727a71fbbb0198157692f1c9d1bd459034
Jalen Peeler <jalenpeeler@yahoo.com>
2021-03-27 03:35:29 -0400

    updated css, added script

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-let h2 = document.createElement('h2')
-h2.textContent = "This content is added by JavaScript"
-
-document.querySelector('body').appendChild(h2)
+function createCardFromCity(city) {
+    const card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = 
+    `<h3>${city.name}</h3>
+    <small>population: ${city.population}</small>
+    <p>You'll want to go see the ${city.attraction}</p>
+    `
+    return card
+  }
